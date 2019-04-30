d181ef66649c84cd26d79a30f6ce1162d933ed30
xatzel <xatzel00@gmail.com>
2019-04-30 12:57:43 -0500

    funciones y test

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+const path = require('path');
+
+function validatePath(pathMd) {
+	const extension = path.extname(pathMd);
+	if (extension == '.md') {
+		console.log('true');
+		return true;
+	}
+}
+
+function absolutePath(absolutelink) {
+	const absolutePt = path.resolve(absolutelink);
+	return absolutePt;
+}
+
+function readAllDocs(nPath) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(nPath, 'utf-8', function(err, data) {
+			if (err) {
+				reject(err);
+			} else {
+				//esto es un codigo asincrono y una funcion callback
+				resolve(data);
+				// console.log(data.datos.toString());
+			}
+		});
+	});
+}
+// este modulo nos permite trabajar e interactuar con los documentos
+// del sistema operativo cuando se crean servidores o programas
+module.exports = {
+	validatePath,
+	absolutePath,
+	readAllDocs
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,25 +1,15 @@
-/*const mdLinks = require('../');
-
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
-})*/
-const { validatePath, absolutePath, readFileMd } = require('../src/mdLink.js');
+const { validatePath, absolutePath, readAllDocs } = require('../index.js');
 
 test('should be a file markdown', () => {
 	expect(validatePath('README.md')).toBe(true);
 });
 
 test('should be a absolute path ', () => {
-	expect(absolutePath('README.md')).toBe('/home/laboratoria-180/Escritorio/GDL002-md-links/README.md');
+	expect(absolutePath('README.md')).toBe('/Users/xary/Documents/laboratoria/GDL002-md-links/README.md');
 });
 
 test('should read a file', () => {
-	readFileMd('prueba.md').then((result) => {
+	readAllDocs('prueba.md').then((result) => {
 		expect(result).toBe('holis a todos');
 	});
 });
